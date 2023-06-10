ad8febb0a76464ba957c5e6f62b7e745d36b6e62
Achmad Ariesta Obos <133846791+achmadariesta@users.noreply.github.com>
2023-06-10 16:53:57 +0800

    Update body.dart

--- lib/screens/login_success/components/body.dart
+++ lib/screens/login_success/components/body.dart
@@ -10,7 +10,7 @@ class Body extends StatelessWidget {
       children: [
         SizedBox(height: SizeConfig.screenHeight * 0.04),
         Image.asset(
-          "assets/images/success.png", #Memunculkan Gambar Centang
+          "assets/images/success.png", // Memunculkan Gambar Success
           height: SizeConfig.screenHeight * 0.4, //40%
         ),
         SizedBox(height: SizeConfig.screenHeight * 0.08),
